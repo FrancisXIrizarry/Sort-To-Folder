a5eb03505fb5888de59296e5a49bea490e4f3f95
FrancisXIrizarry <tuxwonder7@gmail.com>
2018-12-28 21:15:31 -0500

    Unix Version/Linux Version complete

--- LinuxVer.h
+++ LinuxVer.h
@@ -17,39 +17,22 @@
 #include <typeinfo>
 #include <algorithm>
 
-
-void checkTypeFunc(std::string fileName, const std::string pathDir, const std::string fullPath){
-    //std::cout << " Filename: " << fileName << "\n pathDir: " << pathDir << "\n fullPath: " << fullPath << std::endl;
-    transform(fileName.begin(), fileName.end(), fileName.begin(), ::tolower);
-    if (fileName.find("pdf") != std::string::npos && fileName != "pdffolder") {
-		std::string folderName = "/pdfFolder/";
-		//addToFolder(fileName, pathFile, pathDir, folderName);
-	}
-	else if (fileName.find("png") != std::string::npos || fileName.find("jpg") != std::string::npos || fileName.find("tiff") != std::string::npos || fileName.find("jpeg") != std::string::npos) {
-
-		std::string folderName = "/picFolder/";
-		//addToFolder(fileName, pathFile, pathDir, folderName);
-	}
-	else if (fileName.find("mp4") != std::string::npos || fileName.find("mov") != std::string::npos || fileName.find("avi") != std::string::npos || fileName.find(".ts") != std::string::npos) {
-
-		std::string folderName = "/vidFolder/";
-		//addToFolder(fileName, pathFile, pathDir, folderName);
-	}
-	else if (fileName.find("zip") != std::string::npos || fileName.find("rar") != std::string::npos  && fileName != "zipfolder") {
-
-		std::string folderName = "/zipFolder/";
-		//addToFolder(fileName, pathFile, pathDir, folderName);
-	}
-	else if (fileName.find(".txt") != std::string::npos || fileName.find(".doc") != std::string::npos || fileName.find(".readme") != std::string::npos
-		|| fileName.find(".text") != std::string::npos&& fileName != "textfolder") {
-
-		std::string folderName = "/textFolder/";
-		//addToFolder(fileName, pathFile, pathDir, folderName);
-	}
-	else {
-		//lastResort(fileName, pathFile, pathDir);
-		//if (!strstr(fileName.c_str(), "folder")) {cout << fileName << endl;}
-	}
+bool createFolder(std::string outputFolder){
+    //std::cout << "outputFolder: " <<  outputFolder << std::endl;
+    DIR* tempDir = opendir(outputFolder.c_str());
+    if(tempDir){
+        closedir (tempDir);
+        return true;
+    }
+    else{
+        const int dir_err = mkdir(outputFolder.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        if (-1 == dir_err)
+        {
+            printf("Error creating directory!!!N\n");
+            return false;
+        }
+        return true;
+    }
 }
 
 void defaultFolderSetup(std::string path)
@@ -62,30 +45,81 @@ void defaultFolderSetup(std::string path)
 
 	std::cout << pdfStr << std::endl;
 	try {
-		/*createFolder(pdfStr);
+		createFolder(pdfStr);
 		createFolder(picStr);
 		createFolder(vidStr);
 		createFolder(zipStr);
-		createFolder(zipStr);*/
+		createFolder(textStr);
 	}
 	catch (...) {
 		std::cerr << "Something broke in Pre-Set Folder Creation Function (checkPathExists)" << std::endl;
 	}
 }
 
-void addToFolder(std::string fileNVar, std::string pathDirVar, std::string folderName){
-        std::string fullPathAddit = pathDirVar + folderName + "/" + fileNVar;
-        const int dir_err = mkdir(fullPathAddit.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        if (-1 == dir_err)
-        {
-            printf("Error creating directory!n");
-            exit(1);
+void addToFolder(std::string fileNVar, std::string pathDirVar, std::string srcpath , std::string folderName){
+        std::string destpath = pathDirVar + folderName + "/" + fileNVar;
+        //std::cout << " \n\n destPath :: " << destpath << std::endl;
+        //std::cout << " \n\n srcpath :: " << srcpath << std::endl;
+        if (rename (srcpath.c_str(), destpath.c_str())) {
+            // something went wrong
+            if (errno == EXDEV) {
+            // copy data and meta data 
+            } 
+            else { perror("rename"); exit(EXIT_FAILURE); };
+            } 
+        else { // the rename succeeded
         }
 }
 
-void createFolder(std::string outputFolder){
-    
+void lastResort(std::string fileName, const std::string srcpath, const std::string pathDir) {
+	size_t found = fileName.find_last_of(".");
+	std::string fileType = fileName.substr(found + 1);
+	std::string folderName = "/" + fileType + "Folder";
+    std::string strLoc = pathDir + folderName + "/" + fileName;
+    std::string strFold = pathDir + folderName;
+    //std::cout << strLoc << "        and         " << strFold << std::endl;
+    if(createFolder(strFold)){
+        addToFolder(fileName, pathDir, srcpath, folderName);
+    }
 }
+
+void checkTypeFunc(std::string fileName, const std::string pathDir, const std::string fullPath){
+    std::cout << " Filename: " << fileName << "\n pathDir: " << pathDir << "\n fullPath: " << fullPath << std::endl;
+    transform(fileName.begin(), fileName.end(), fileName.begin(), ::tolower);
+    if (fileName.find("pdf") != std::string::npos && fileName != "pdffolder") {
+		std::string folderName = "/pdfFolder/";
+		addToFolder(fileName, pathDir, fullPath, folderName);
+	}
+	else if (fileName.find("png") != std::string::npos || fileName.find("jpg") != std::string::npos || fileName.find("tiff") != std::string::npos || fileName.find("jpeg") != std::string::npos) {
+
+		std::string folderName = "/picFolder/";
+		addToFolder(fileName, pathDir, fullPath, folderName);
+	}
+	else if (fileName.find("mp4") != std::string::npos || fileName.find("mov") != std::string::npos || fileName.find("avi") != std::string::npos || fileName.find(".ts") != std::string::npos) {
+
+		std::string folderName = "/vidFolder/";
+		addToFolder(fileName, pathDir, fullPath, folderName);
+	}
+	else if ((fileName.find("zip") != std::string::npos || fileName.find("rar") != std::string::npos)  && fileName != "zipfolder") {
+
+		std::string folderName = "/zipFolder/";
+  
+		addToFolder(fileName, pathDir, fullPath, folderName);
+	}
+	else if ((fileName.find(".txt") != std::string::npos || fileName.find(".doc") != std::string::npos || fileName.find(".readme") != std::string::npos
+		|| fileName.find(".text") != std::string::npos) && fileName != "textfolder") {
+
+		std::string folderName = "/textFolder/";
+		addToFolder(fileName, pathDir, fullPath, folderName);
+	}
+	else {
+		lastResort(fileName, fullPath, pathDir);
+		//if (!strstr(fileName.c_str(), "folder")) {cout << fileName << endl;}
+	}
+}
+
+
+
 
 
 
@@ -97,13 +131,13 @@ int mainLinuxFunc(std::string PLAT_FORM)
     std::cout << "Works (Linux)" << std::endl;
     struct passwd *pw = getpwuid(getuid());
     std::string homedir = pw->pw_dir;
-    homedir = homedir + "/Downloads/";
+    homedir = homedir + "/Downloads";
     std::cout << homedir << std::endl;
 
 
     DIR *dir;
     //checkType(dir);
-    
+    defaultFolderSetup(homedir);
     struct dirent *entity;
     if ((dir = opendir (homedir.c_str())) != NULL) {
         /* print all the files and directories within directory */
@@ -116,7 +150,7 @@ int mainLinuxFunc(std::string PLAT_FORM)
             }
             else{
                 //std::cout << entity->d_name << std::endl;
-                std::string fullPathVar = homedir +  entity->d_name;
+                std::string fullPathVar = homedir + "/" + entity->d_name;
                 checkTypeFunc(entity->d_name, homedir, fullPathVar);
                 
             }
@@ -129,7 +163,7 @@ int mainLinuxFunc(std::string PLAT_FORM)
         perror ("");
         return EXIT_FAILURE;
     }
-    
+    return 0;
 }
 
 
