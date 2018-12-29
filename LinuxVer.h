31526126c785f8b6f50816599f06d4cf4eb364be
FrancisXIrizarry <tuxwonder7@gmail.com>
2018-12-28 20:01:24 -0500

    Added macro defines for system type, and some experimental code

--- LinuxVer.h
+++ LinuxVer.h
@@ -1,11 +1,43 @@
 #pragma once
 
+#ifdef __unix
+
+
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
+#include <dirent.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-int mainWindowFunc(std::string PLAT_FORM)
+int mainLinuxFunc(std::string PLAT_FORM)
 {
+    std::cout << "Works (Linux)" << std::endl;
+    struct passwd *pw = getpwuid(getuid());
+    std::string homedir = pw->pw_dir;
+    homedir = homedir + "/Downloads/";
+    std::cout << homedir << std::endl;
 
-	return 0;
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (homedir.c_str())) != NULL) {
+        /* print all the files and directories within directory */
+        while ((ent = readdir (dir)) != NULL) {
+            printf ("%s\n", ent->d_name);
+        }
+        closedir (dir);
+    }   
+    else {
+        /* could not open directory */
+        perror ("");
+        return EXIT_FAILURE;
+    }
+    
 }
+
+
+#endif
